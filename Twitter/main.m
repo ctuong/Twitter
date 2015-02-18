c019c3c1d6b430ec73e9a91da4b2c5cebe20c4d9
Calvin Tuong <ctuong@box.com>
2015-02-17 22:58:56 -0800

    Initial Commit

---
+++ Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Calvin Tuong on 2/17/15.
+//  Copyright (c) 2015 Calvin Tuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
